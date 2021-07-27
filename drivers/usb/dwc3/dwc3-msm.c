642aa77a59651310e1e725cc60cfb5a07a130c00
Alexander Koskovich <zvnexus@outlook.com>
2021-07-27 00:54:05 -0700

    fix
    Change-Id: If60cafda1f40f0bf5fcb4ee0acd7ac71074c762a

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -4480,7 +4480,6 @@ static ssize_t mode_store(struct device *dev, struct device_attribute *attr,
 		const char *buf, size_t count)
 {
 	struct dwc3_msm *mdwc = dev_get_drvdata(dev);
-	struct dwc3 *dwc = platform_get_drvdata(mdwc->dwc3);
 
 #ifdef CONFIG_MACH_ASUS
 	if (setModefromPOGO) {
