22f37205da2bef982dff801742bee7ee86b129c1
Juhyung Park <qkrwngud825@gmail.com>
2021-08-14 00:02:21 +0900

    qcacld: resolve duplicated symbols
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Change-Id: I8deb833786903bb5e684aa69bd7af4313c1308c6

--- drivers/staging/qcacld-3.0/components/ipa/dispatcher/src/wlan_ipa_obj_mgmt_api.c
+++ drivers/staging/qcacld-3.0/components/ipa/dispatcher/src/wlan_ipa_obj_mgmt_api.c
@@ -25,16 +25,9 @@
 #include "target_if_ipa.h"
 #include "wlan_ipa_ucfg_api.h"
 
-static bool g_ipa_is_ready;
-bool ipa_is_ready(void)
-{
-	return g_ipa_is_ready;
-}
-
 void ipa_disable_register_cb(void)
 {
 	ipa_debug("Don't register ready cb with IPA driver");
-	g_ipa_is_ready = false;
 }
 
 /**
@@ -144,7 +137,6 @@ static void ipa_register_ready_cb(void *user_data)
 		return;
 	}
 
-	g_ipa_is_ready = true;
 	ipa_info("IPA ready callback invoked: ipa_register_ready_cb");
 	ipa_obj = ipa_pdev_get_priv_obj(pdev);
 	status = ipa_obj_setup(ipa_obj);
